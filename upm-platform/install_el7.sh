59fbc68896042e339d568addf28ac4eeb37ff7b8
鲍琳 <johnny.beaus@gmail.com>
2023-10-20 16:57:37 +0800

    Update install_el7.sh

--- upm-platform/install_el7.sh
+++ upm-platform/install_el7.sh
@@ -139,10 +139,10 @@ install_upm_platform() {
     --set-string nginx.nodeAffinityPreset.type="hard" \
     --set-string nginx.nodeAffinityPreset.key="upm\.platform\.node" \
     --set-string nginx.nodeAffinityPreset.values='{enable}' \
-    --set-string nginx.resources.limits.cpu="${PLATFORM_RESOURCE_LIMITS_CPU}" \
-    --set-string nginx.resources.limits.memory="${PLATFORM_RESOURCE_LIMITS_MEMORY}" \
-    --set-string nginx.resources.requests.cpu="${PLATFORM_RESOURCE_REQUESTS_CPU}" \
-    --set-string nginx.resources.requests.memory="${PLATFORM_RESOURCE_REQUESTS_MEMORY}" \
+    --set-string nginx.resources.limits.cpu="1000m" \
+    --set-string nginx.resources.limits.memory="2Gi" \
+    --set-string nginx.resources.requests.cpu="1000m" \
+    --set-string nginx.resources.requests.memory="2Gi" \
     --set-string ui.image.tag="${UI_VERSION}" \
     --set ui.replicaCount="${PLATFORM_NODE_COUNT}" \
     --set-string ui.nodeAffinityPreset.type="hard" \
