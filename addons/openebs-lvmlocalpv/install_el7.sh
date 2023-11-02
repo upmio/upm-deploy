0ee0d4eb2bd9a8f5521c6471b7451e5af6700727
鲍琳 <johnny.beaus@gmail.com>
2023-11-02 13:01:57 +0800

    Update install_el7.sh

--- addons/openebs-lvmlocalpv/install_el7.sh
+++ addons/openebs-lvmlocalpv/install_el7.sh
@@ -22,9 +22,12 @@
 readonly CHART="openebs-lvmlocalpv/lvm-localpv"
 readonly RELEASE="openebs-lvmlocalpv"
 readonly TIME_OUT_SECOND="600s"
-readonly VERSION="1.3.0"
+readonly CHART_VERSION="1.3.0"
 
+OFFLINE_INSTALL="${OFFLINE_INSTALL:-false}"
 OPENEBS_KUBE_NAMESPACE="${OPENEBS_KUBE_NAMESPACE:-openebs}"
+INSTALL_LOG_PATH=/tmp/openebs-lvmlocalpv_install-$(date +'%Y-%m-%d_%H-%M-%S').log
+
 OPENEBS_CONTROLLER_RESOURCE_LIMITS_CPU="${OPENEBS_CONTROLLER_RESOURCE_LIMITS_CPU:-500m}"
 OPENEBS_CONTROLLER_RESOURCE_LIMITS_MEMORY="${OPENEBS_CONTROLLER_RESOURCE_LIMITS_MEMORY:-512Mi}"
 OPENEBS_CONTROLLER_RESOURCE_REQUESTS_CPU="${OPENEBS_CONTROLLER_RESOURCE_REQUESTS_CPU:-500m}"
@@ -33,7 +36,6 @@ OPENEBS_NODE_RESOURCE_LIMITS_CPU="${OPENEBS_NODE_RESOURCE_LIMITS_CPU:-500m}"
 OPENEBS_NODE_RESOURCE_LIMITS_MEMORY="${OPENEBS_NODE_RESOURCE_LIMITS_MEMORY:-512Mi}"
 OPENEBS_NODE_RESOURCE_REQUESTS_CPU="${OPENEBS_NODE_RESOURCE_REQUESTS_CPU:-500m}"
 OPENEBS_NODE_RESOURCE_REQUESTS_MEMORY="${OPENEBS_NODE_RESOURCE_REQUESTS_MEMORY:-512Mi}"
-INSTALL_LOG_PATH=""
 
 info() {
   echo "[Info][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
@@ -44,38 +46,24 @@ error() {
   exit 1
 }
 
-install_kubectl() {
-  info "Install kubectl..."
-  if ! curl -LOs "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"; then
-    error "Fail to get kubectl, please confirm whether the connection to dl.k8s.io is ok?"
-  fi
-  if ! sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl; then
-    error "Install kubectl fail"
-  fi
-  info "Kubectl install completed"
+installed() {
+  command -v "$1" >/dev/null 2>&1
 }
 
-install_helm() {
-  info "Install helm..."
-  if ! curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3; then
-    error "Fail to get helm installed script, please confirm whether the connection to raw.githubusercontent.com is ok?"
-  fi
-  chmod 700 get_helm.sh
-  if ! ./get_helm.sh; then
-    error "Fail to get helm when running get_helm.sh"
-  fi
-  info "Helm install completed"
-}
-
-install_lvmlocalpv() {
+online_install_lvmlocalpv() {
   # check if openebs-lvmlocalpv already installed
   if helm status ${RELEASE} -n "${OPENEBS_KUBE_NAMESPACE}" &>/dev/null; then
     error "${RELEASE} already installed. Use helm remove it first"
   fi
+
+  info "Start add helm openebs-lvmlocalpv repo"
+  helm repo add openebs-lvmlocalpv https://openebs.github.io/lvm-localpv &>/dev/null || error "Helm add openebs-lvmlocalpv repo error."
+  info "Start update helm openebs-lvmlocalpv repo"
+  helm repo update openebs-lvmlocalpv 2>/dev/null || error "Helm update openebs-lvmlocalpv repo error."
+
   info "Install openebs-lvmlocalpv, It might take a long time..."
   helm install ${RELEASE} ${CHART} \
-    --debug \
-    --version "${VERSION}" \
+    --version "${CHART_VERSION}" \
     --namespace "${OPENEBS_KUBE_NAMESPACE}" \
     --create-namespace \
     --set lvmController.nodeSelector."openebs\.io/control-plane"="enable" \
@@ -91,41 +79,61 @@ install_lvmlocalpv() {
     --set lvmPlugin.allowedTopologies='kubernetes\.io/hostname\,openebs\.io/node' \
     --set analytics.enabled=false \
     --timeout $TIME_OUT_SECOND \
-    --wait 2>&1 | grep "\[debug\]" | awk '{$1="[Helm]"; $2=""; print }' | tee -a "${INSTALL_LOG_PATH}" || {
+    --wait 2>&1 | tee -a "${INSTALL_LOG_PATH}" || {
     error "Fail to install ${RELEASE}."
   }
 
   #TODO: check more resources after install
 }
 
-init_helm_repo() {
-  helm repo add openebs-lvmlocalpv https://openebs.github.io/lvm-localpv &>/dev/null
-  info "Start update helm openebs-lvmlocalpv repo"
-  if ! helm repo update openebs-lvmlocalpv 2>/dev/null; then
-    error "Helm update openebs-lvmlocalpv repo error."
+offline_install_lvmlocalpv() {
+  # check if openebs-lvmlocalpv already installed
+  if helm status ${RELEASE} -n "${OPENEBS_KUBE_NAMESPACE}" &>/dev/null; then
+    error "${RELEASE} already installed. Use helm remove it first"
   fi
+
+  [[ -d "${OPENEBS_CHART_DIR}" ]] || error "OPENEBS_CHART_DIR not exist."
+
+  info "Install openebs-lvmlocalpv, It might take a long time..."
+  helm install ${RELEASE} "${OPENEBS_CHART_DIR}" \
+    --namespace "${OPENEBS_KUBE_NAMESPACE}" \
+    --create-namespace \
+    --set-string lvmController.resizer.image.registry="${IMAGE_REGISTRY}" \
+    --set-string lvmController.snapshotter.image.registry="${IMAGE_REPOSITORY}" \
+    --set-string lvmController.snapshotController.image.registry="${IMAGE_REGISTRY}" \
+    --set-string lvmController.provisioner.image.registry="${IMAGE_REGISTRY}" \
+    --set lvmController.nodeSelector."openebs\.io/control-plane"="enable" \
+    --set-string lvmController.resources.limits.cpu="${OPENEBS_CONTROLLER_RESOURCE_LIMITS_CPU}" \
+    --set-string lvmController.resources.limits.memory="${OPENEBS_CONTROLLER_RESOURCE_LIMITS_MEMORY}" \
+    --set-string lvmController.resources.requests.cpu="${OPENEBS_CONTROLLER_RESOURCE_REQUESTS_CPU}" \
+    --set-string lvmController.resources.requests.memory="${OPENEBS_CONTROLLER_RESOURCE_REQUESTS_MEMORY}" \
+    --set-string lvmNode.driverRegistrar.image.registry="${IMAGE_REGISTRY}" \
+    --set lvmNode.nodeSelector."openebs\.io/node"="enable" \
+    --set-string lvmNode.resources.limits.cpu="${OPENEBS_NODE_RESOURCE_LIMITS_CPU}" \
+    --set-string lvmNode.resources.limits.memory="${OPENEBS_NODE_RESOURCE_LIMITS_MEMORY}" \
+    --set-string lvmNode.resources.requests.cpu="${OPENEBS_NODE_RESOURCE_REQUESTS_CPU}" \
+    --set-string lvmNode.resources.requests.memory="${OPENEBS_NODE_RESOURCE_REQUESTS_MEMORY}" \
+    --set-string lvmPlugin.image.registry="${IMAGE_REGISTRY}" \
+    --set lvmPlugin.allowedTopologies='kubernetes\.io/hostname\,openebs\.io/node' \
+    --set analytics.enabled=false \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${RELEASE}."
+  }
+
+  #TODO: check more resources after install
 }
 
 verify_supported() {
-  local HAS_HELM
-  HAS_HELM="$(type "helm" &>/dev/null && echo true || echo false)"
-  local HAS_KUBECTL
-  HAS_KUBECTL="$(type "kubectl" &>/dev/null && echo true || echo false)"
-  local HAS_CURL
-  HAS_CURL="$(type "curl" &>/dev/null && echo true || echo false)"
+  installed helm || error "helm is required"
+  installed kubectl || error "kubectl is required"
+  installed curl || error "curl is required"
+  installed yq || error "yq is required"
 
-  if [[ -z "${OPENEBS_STORAGECLASS_NAME}" ]]; then
-    error "OPENEBS_STORAGECLASS_NAME MUST set in environment variable."
-  fi
+  [[ -n "${OPENEBS_STORAGECLASS_NAME}" ]] || error "OPENEBS_STORAGECLASS_NAME MUST set in environment variable."
+  [[ -n "${OPENEBS_VG_NAME}" ]] || error "OPENEBS_VG_NAME MUST set in environment variable."
 
-  if [[ -z "${OPENEBS_VG_NAME}" ]]; then
-    error "OPENEBS_VG_NAME MUST set in environment variable."
-  fi
-
-  if [[ -z "${OPENEBS_CONTROLLER_NODE_NAMES}" ]]; then
-    error "OPENEBS_CONTROLLER_NODE_NAMES MUST set in environment variable."
-  fi
-
+  [[ -n "${OPENEBS_CONTROLLER_NODE_NAMES}" ]] || error "OPENEBS_CONTROLLER_NODE_NAMES MUST set in environment variable."
   local node
   local control_node_array
   IFS="," read -r -a control_node_array <<<"${OPENEBS_CONTROLLER_NODE_NAMES}"
@@ -134,11 +142,7 @@ verify_supported() {
       error "kubectl label node ${node} 'openebs.io/control-plane=enable' failed, use kubectl to check reason"
     }
   done
-
-  if [[ -z "${OPENEBS_DATA_NODE_NAMES}" ]]; then
-    error "OPENEBS_DATA_NODE_NAMES MUST set in environment variable."
-  fi
-
+  [[ -n "${OPENEBS_DATA_NODE_NAMES}" ]] || error "OPENEBS_DATA_NODE_NAMES MUST set in environment variable."
   local data_node_array
   IFS="," read -r -a data_node_array <<<"${OPENEBS_DATA_NODE_NAMES}"
   for node in "${data_node_array[@]}"; do
@@ -146,25 +150,10 @@ verify_supported() {
       error "kubectl label node ${node} 'openebs.io/node=enable' failed, use kubectl to check reason"
     }
   done
-
-  if [[ "${HAS_CURL}" != "true" ]]; then
-    error "curl is required"
-  fi
-
-  if [[ "${HAS_HELM}" != "true" ]]; then
-    install_helm
-  fi
-
-  if [[ "${HAS_KUBECTL}" != "true" ]]; then
-    install_kubectl
-  fi
 }
 
 init_log() {
-  INSTALL_LOG_PATH=/tmp/openebs-lvmlocalpv_install-$(date +'%Y-%m-%d_%H-%M-%S').log
-  if ! touch "${INSTALL_LOG_PATH}"; then
-    error "Create log file ${INSTALL_LOG_PATH} error"
-  fi
+  touch "${INSTALL_LOG_PATH}" || error "Create log file ${INSTALL_LOG_PATH} error"
   info "Log file create in path ${INSTALL_LOG_PATH}"
 }
 
@@ -175,8 +164,10 @@ init_log() {
 #   namespace
 ############################################
 verify_installed() {
-  helm status "${RELEASE}" -n "${OPENEBS_KUBE_NAMESPACE}" | grep deployed &>/dev/null || {
-    error "${RELEASE} installed fail, check log use helm and kubectl."
+  local status
+  status=$(helm status "${RELEASE}" -n "${NACOS_KUBE_NAMESPACE}" -o yaml | yq -r '.info.status')
+  [[ "${status}" == "deployed" ]] || {
+    error "Helm release ${RELEASE} status is not deployed, use helm to check reason"
   }
 
   info "${RELEASE} Deployment Completed!"
@@ -194,8 +185,11 @@ create_storageclass() {
 main() {
   init_log
   verify_supported
-  init_helm_repo
-  install_lvmlocalpv
+  if [[ ${OFFLINE_INSTALL} == "false" ]]; then
+    online_install_lvmlocalpv
+  elif [[ ${OFFLINE_INSTALL} == "true" ]]; then
+    offline_install_lvmlocalpv
+  fi
   verify_installed
   create_storageclass
 }
