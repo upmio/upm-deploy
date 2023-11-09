3fb3d37519b3ce35a60c372a1e62dc939a664b25
鲍琳 <johnny.beaus@gmail.com>
2023-11-09 17:42:43 +0800

    add crossplane

---
+++ addons/crossplane/README.md
@@ -0,0 +1,51 @@
+# 部署 crossplane
+
+本文档目标是指导安装多集群管理程序 [crossplane](https://github.com/crossplane/crossplane) 。
+
+## 外置数据库-快速安装 crossplane 
+
+### 1. 确定登录kubernetes 
+
+使用 kubectl 确定目前连接的集群信息。
+
+```console
+kubectl cluster-info
+```
+
+### 2. 设置必要的环境变量
+
+CROSSPLANE_NODE_NAMES：指定安装 crossplane-controller pod的节点名称，节点名称可以使用","作为分隔符，表示多个节点名称，安装程序会对节点进行label固定安装节点。
+
+CROSSPLANE_KUBE_NAMESPACE：指定安装命名空间，非必填项，默认值为`crossplane-system`。
+```console
+export CROSSPLANE_NODE_NAMES="crossplane-control-plan01"
+```
+
+### 3. 运行安装脚本
+
+**注意⚠️：如果找不到 Helm3，将自动安装。**
+
+**注意⚠️：安装脚本会对指定节点进行添加label的操作。**
+
+运行安装脚本
+```console
+# BASH
+curl -sSL https://raw.githubusercontent.com/upmio/upm-deploy/main/addons/crossplane/install_el7.sh | sh -
+```
+
+等待几分钟。 如果所有 crossplane pod 都在运行，则 crossplane 将成功安装。
+
+```console
+kubectl get pods -n crossplane-system  -w
+```
+
+## 使用 Helm 卸载 crossplane
+
+```console
+# Helm
+helm uninstall crossplane -n crossplane-system
+```
+
+这将删除与 Charts 关联的所有 Kubernetes 组件并删除发布。
+
+_请参阅 [helm uninstall](https://helm.sh/docs/helm/helm_uninstall/) 获取命令文档。_
---
+++ addons/crossplane/install_el7.sh
@@ -0,0 +1,174 @@
+#!/usr/bin/env bash
+
+# You must be prepared as follows before run install.sh:
+#
+# 1. CROSSPLANE_NODE_NAMES MUST be set as environment variable, for an example:
+#
+#        export CROSSPLANE_NODE_NAMES="master01,master02"
+#
+
+readonly CHART="crossplane-stable/crossplane"
+readonly RELEASE="crossplane"
+readonly TIME_OUT_SECOND="600s"
+readonly CHART_VERSION="1.14.0"
+
+OFFLINE_INSTALL="${OFFLINE_INSTALL:-false}"
+CROSSPLANE_KUBE_NAMESPACE="${CROSSPLANE_KUBE_NAMESPACE:-crossplane-system}"
+CROSSPLANE_RESOURCE_LIMITS="${CROSSPLANE_RESOURCE_LIMITS:-1}"
+INSTALL_LOG_PATH=/tmp/crossplane_install-$(date +'%Y-%m-%d_%H-%M-%S').log
+
+if [[ ${CROSSPLANE_RESOURCE_LIMITS} -eq 0 ]]; then
+  CROSSPLANE_RESOURCE_LIMITS_CPU="0"
+  CROSSPLANE_RESOURCE_LIMITS_MEMORY="0"
+  CROSSPLANE_RESOURCE_REQUESTS_CPU="0"
+  CROSSPLANE_RESOURCE_REQUESTS_MEMORY="0"
+elif [[ ${CROSSPLANE_RESOURCE_LIMITS} -gt 0 && ${CROSSPLANE_RESOURCE_LIMITS} -le 4 ]]; then
+  CROSSPLANE_RESOURCE_LIMITS_CPU="1000m"
+  CROSSPLANE_RESOURCE_LIMITS_MEMORY="${CROSSPLANE_RESOURCE_LIMITS}Gi"
+  CROSSPLANE_RESOURCE_REQUESTS_CPU="1000m"
+  CROSSPLANE_RESOURCE_REQUESTS_MEMORY="${CROSSPLANE_RESOURCE_LIMITS}Gi"
+elif [[ ${CROSSPLANE_RESOURCE_LIMITS} -gt 4 ]]; then
+  CROSSPLANE_RESOURCE_LIMITS_CPU="2000m"
+  CROSSPLANE_RESOURCE_LIMITS_MEMORY="${CROSSPLANE_RESOURCE_LIMITS}Gi"
+  CROSSPLANE_RESOURCE_REQUESTS_CPU="2000m"
+  CROSSPLANE_RESOURCE_REQUESTS_MEMORY="${CROSSPLANE_RESOURCE_LIMITS}Gi"
+fi
+
+info() {
+  echo "[Info][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+}
+
+error() {
+  echo "[Error][$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" | tee -a "${INSTALL_LOG_PATH}"
+  exit 1
+}
+
+installed() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+online_install_crossplane() {
+  info "Install crossplane..."
+  # check if crossplane already installed
+  if helm status "${RELEASE}" -n "${CROSSPLANE_KUBE_NAMESPACE}" &>/dev/null; then
+    error "${RELEASE} already installed. Use helm remove it first"
+  fi
+
+  info "Start init helm crossplane repo"
+  helm repo add crossplane-stable https://charts.crossplane.io/stable &>/dev/null || {
+    error "Helm repo add crossplane error."
+  }
+  info "Start update helm crossplane repo"
+  if ! helm repo update crossplane-stable 2>/dev/null; then
+    error "Helm update crossplane repo error."
+  fi
+
+  info "Install ${RELEASE}, It might take a long time..."
+  helm install "${RELEASE}" "${CHART}" \
+    --version "${CHART_VERSION}" \
+    --namespace "${CROSSPLANE_KUBE_NAMESPACE}" \
+    --create-namespace \
+    --set image.repository="crossplane/crossplane" \
+    --set replicas="${CROSSPLANE_NODE_COUNT}" \
+    --set nodeSelector."crossplane\.io/control-plane"="enable" \
+    --set rbacManager.replicas="${CROSSPLANE_NODE_COUNT}" \
+    --set rbacManager.nodeSelector."crossplane\.io/control-plane"="enable" \
+    --set resourcesCrossplane.limits.cpu="${CROSSPLANE_RESOURCE_LIMITS_CPU}" \
+    --set resourcesCrossplane.limits.memory="${CROSSPLANE_RESOURCE_LIMITS_MEMORY}" \
+    --set resourcesCrossplane.requests.cpu="${CROSSPLANE_RESOURCE_REQUESTS_CPU}" \
+    --set resourcesCrossplane.requests.memory="${CROSSPLANE_RESOURCE_REQUESTS_MEMORY}" \
+    --set resourcesRBACManager.limits.cpu="${CROSSPLANE_RESOURCE_LIMITS_CPU}" \
+    --set resourcesRBACManager.limits.memory="${CROSSPLANE_RESOURCE_LIMITS_MEMORY}" \
+    --set resourcesRBACManager.requests.cpu="${CROSSPLANE_RESOURCE_REQUESTS_CPU}" \
+    --set resourcesRBACManager.requests.memory="${CROSSPLANE_RESOURCE_REQUESTS_MEMORY}" \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${RELEASE}."
+  }
+
+  #TODO: check more resources after install
+}
+
+offline_install_crossplane() {
+  info "Install crossplane..."
+  # check if crossplane already installed
+  if helm status "${RELEASE}" -n "${CROSSPLANE_KUBE_NAMESPACE}" &>/dev/null; then
+    error "${RELEASE} already installed. Use helm remove it first"
+  fi
+
+  [[ -d "${CROSSPLANE_CHART_DIR}" ]] || error "CROSSPLANE_CHART_DIR not exist."
+
+  info "Install ${RELEASE}, It might take a long time..."
+  helm install "${RELEASE}" "${CROSSPLANE_CHART_DIR}" \
+    --namespace "${CROSSPLANE_KUBE_NAMESPACE}" \
+    --create-namespace \
+    --set image.repository="${MYSQL_IMAGE_REGISTRY}/crossplane/crossplane" \
+    --set replicas="${CROSSPLANE_NODE_COUNT}" \
+    --set nodeSelector."crossplane\.io/control-plane"="enable" \
+    --set rbacManager.replicas="${CROSSPLANE_NODE_COUNT}" \
+    --set rbacManager.nodeSelector."crossplane\.io/control-plane"="enable" \
+    --set resourcesCrossplane.limits.cpu="${CROSSPLANE_RESOURCE_LIMITS_CPU}" \
+    --set resourcesCrossplane.limits.memory="${CROSSPLANE_RESOURCE_LIMITS_MEMORY}" \
+    --set resourcesCrossplane.requests.cpu="${CROSSPLANE_RESOURCE_REQUESTS_CPU}" \
+    --set resourcesCrossplane.requests.memory="${CROSSPLANE_RESOURCE_REQUESTS_MEMORY}" \
+    --set resourcesRBACManager.limits.cpu="${CROSSPLANE_RESOURCE_LIMITS_CPU}" \
+    --set resourcesRBACManager.limits.memory="${CROSSPLANE_RESOURCE_LIMITS_MEMORY}" \
+    --set resourcesRBACManager.requests.cpu="${CROSSPLANE_RESOURCE_REQUESTS_CPU}" \
+    --set resourcesRBACManager.requests.memory="${CROSSPLANE_RESOURCE_REQUESTS_MEMORY}" \
+    --timeout $TIME_OUT_SECOND \
+    --wait 2>&1 | tee -a "${INSTALL_LOG_PATH}" || {
+    error "Fail to install ${RELEASE}."
+  }
+
+  #TODO: check more resources after install
+}
+
+verify_supported() {
+  installed helm || error "helm is required"
+  installed kubectl || error "kubectl is required"
+  installed curl || error "curl is required"
+
+  [[ -n "${CROSSPLANE_NODE_NAMES}" ]] || error "CROSSPLANE_NODE_NAMES MUST set in environment variable."
+  local node
+  local node_array
+  IFS="," read -r -a node_array <<<"${CROSSPLANE_NODE_NAMES}"
+  CROSSPLANE_NODE_COUNT=0
+  for node in "${node_array[@]}"; do
+    kubectl label node "${node}" 'crossplane.io/control-plane=enable' --overwrite &>/dev/null || {
+      error "kubectl label node ${node} 'crossplane.io/control-plane=enable' failed, use kubectl to check reason"
+    }
+    ((CROSSPLANE_NODE_COUNT++))
+  done
+}
+
+init_log() {
+  touch "${INSTALL_LOG_PATH}" || error "Create log file ${INSTALL_LOG_PATH} error"
+  info "Log file create in path ${INSTALL_LOG_PATH}"
+}
+
+############################################
+# Check if helm release deployment correctly
+# Arguments:
+#   release
+#   namespace
+############################################
+verify_installed() {
+  local status
+  status=$(helm status "${RELEASE}" -n "${CROSSPLANE_KUBE_NAMESPACE}" -o yaml | yq -r '.info.status')
+  [[ "${status}" == "deployed" ]] || error "Helm release ${RELEASE} status is not deployed, use helm to check reason"
+
+  info "${RELEASE} Deployment Completed!"
+}
+
+main() {
+  init_log
+  verify_supported
+  if [[ ${OFFLINE_INSTALL} == "false" ]]; then
+    online_install_crossplane
+  elif [[ ${OFFLINE_INSTALL} == "true" ]]; then
+    offline_install_crossplane
+  fi
+  verify_installed
+}
+
+main
