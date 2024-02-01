3d73782db3b596452a249c97ff68905522d5271b
鲍琳 <johnny.beaus@gmail.com>
2024-02-01 13:42:23 +0800

    Update install_el7.sh

--- upm-platform/install_el7.sh
+++ upm-platform/install_el7.sh
@@ -60,7 +60,7 @@ readonly RELEASE="upm-platform"
 readonly TIME_OUT_SECOND="600s"
 readonly CHART_VERSION="1.1.0"
 readonly UI_VERSION="dev-cdd0b9ad"
-readonly API_VERSION="dev-346a6082"
+readonly API_VERSION="dev-9f03f126"
 
 OFFLINE_INSTALL="${OFFLINE_INSTALL:-false}"
 PLATFORM_INIT_DB="${PLATFORM_INIT_DB:-false}"
