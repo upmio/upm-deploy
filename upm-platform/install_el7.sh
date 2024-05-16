a198e0a998f8ad4bb4942809d57ee22d0e41818e
linleybao <johnny.beaus@gmail.com>
2024-05-16 09:26:03 +0800

    Update install_el7.sh

--- upm-platform/install_el7.sh
+++ upm-platform/install_el7.sh
@@ -511,12 +511,6 @@ init_log() {
   info "Log file create in path ${INSTALL_LOG_PATH}"
 }
 
-############################################
-# Check if helm release deployment correctly
-# Arguments:
-#   release
-#   namespace
-############################################
 verify_installed() {
   local status
   status=$(helm status "${RELEASE}" -n "${PLATFORM_KUBE_NAMESPACE}" -o yaml | yq -r '.info.status')
